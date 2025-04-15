fcd91603faf688405e72a8477d8c32d1fcddea30
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-04-16 00:17:27 +0800

    Update About.jsx

--- src/components/Desktop/About/About.jsx
+++ src/components/Desktop/About/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div className="space-y-4 pb-20"> {/* Add bottom padding too */}
+    <div className="space-y-4 pb-20"> 
       <h2 className="text-2xl font-bold">About Us</h2>
 
     </div>
