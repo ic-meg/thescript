d41003fec8d5c13e38839f1ccc9f330c9e7fce0c
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-04-16 00:11:34 +0800

    members-updatee

--- src/components/Desktop/Members/Members.jsx
+++ src/components/Desktop/Members/Members.jsx
@@ -2,15 +2,9 @@ import React from 'react';
 
 const Members = () => {
     return (
-        <div className="space-y-4 pb-20"> {/* Add bottom padding too */}
-        <h2 className="text-2xl font-bold">About Us</h2>
-        <p>
-          We are The &lt;Script&gt; team. Passionate about tech, retro design, and innovation.
-        </p>
-        {/* Add a bunch of content to force scroll */}
-        {[...Array(30)].map((_, i) => (
-          <p key={i}>This is test content line #{i + 1}</p>
-        ))}
+        <div className="space-y-4 pb-20"> 
+        <h2 className="text-2xl font-bold">Member's Content</h2>
+
       </div>
     );
 };
