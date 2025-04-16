6e3bd01286b9f4fed57471a1bb1922ba263f66de
Ley <165410580+ley2003@users.noreply.github.com>
2025-04-16 22:03:23 +0800

    Update MonitorFrame.jsx

--- src/components/MonitorFrame.jsx
+++ src/components/MonitorFrame.jsx
@@ -3,7 +3,12 @@ import monitor from '../assets/images/apple.png';
 
 const MonitorFrame = ({ children }) => {
   return (
-    <div className="w-full min-h-screen bg-[#eaeada] flex items-center justify-center">
+    <div
+      className="w-full min-h-screen flex items-center justify-center"
+      style={{
+          background: 'radial-gradient(circle,rgba(0, 0, 0, 1) 10%, rgba(119, 119, 140, 1) 100%, rgba(0, 0, 0, 1) 100%)',
+      }}
+    >
       <div
         className="relative w-full max-w-[1280px] aspect-[16/10] bg-cover bg-no-repeat"
         style={{ backgroundImage: `url(${monitor})` }}
