8ce4a79a62c55d6ae5a08161a47bb30adcd202e4
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-04-18 22:13:14 +0800

    kate

--- src/components/Desktop/Members/Kate.jsx
+++ src/components/Desktop/Members/Kate.jsx
@@ -1,37 +1,41 @@
 import React from 'react';
-import './Kate.css';
+import MemberProfile from './MemberProfile';
+
+import avatar from '../../../assets/icons/AVATAR-GUIL.svg';
+import button1 from '../../../assets/icons/button1.png';
+import button2 from '../../../assets/icons/BUTTON-2.svg';
+import button3 from '../../../assets/icons/BUTTON-3.svg';
+import button4 from '../../../assets/icons/BUTTON-4.svg';
+
+import HTML from '../../../assets/icons/HTML.svg';
+import CSS from '../../../assets/icons/CSS.svg';
+import JavaScript from '../../../assets/icons/JAVASCRIPT.svg';
+import SQL from '../../../assets/icons/MYSQL.svg';
+import Figma from '../../../assets/icons/FIGMA.svg';
 
 const Kate = () => {
-    return (
-        <div class="body">
-            <h1>Kate's Profile</h1><br></br>
-            <div class="header">
-                <img scr="/Members/AVATAR-GUIL.png" alt='GUIL'class ="avatar" ></img>
-                <div class="info">
-                    <h1>Guillani calais</h1>
-                    <p>front Developer, Designer</p>
-                    <a href='guilcalais@gmail.com'>Guillanicalais@gmail.com</a>
-                </div>
-            </div><br></br>
-
-            <div class="buttons">
-                <button><img src="BUTTON-1.png"></img></button>
-                <button><img src="BUTTON-2.png"></img></button>
-                <button><img src="BUTTON-3.png"></img></button>
-                <button><img src="BUTTON-4.png"></img></button>
-            </div>
-
-            <div class="skills">
-                <div><img src="HTML.png" alt="HTML"></img></div>
-                <div><img src="CSS.png" alt="CSS"></img></div>
-                <div><img src="JavaScript.png" alt="JavaScript"></img></div>
-                <div><img src="MySQL.png" alt="MySQL"></img></div>
-                <div><img src="BLENDER.png" alt="Blender"></img></div>
-                <div><img src="FIGMA.png" alt="Figma"></img></div>
-                <div><img src="PINTEREST.png" alt="Pinterest"></img></div>
-            </div>
-        </div>
-    );
+  return (
+    <MemberProfile
+      name="Kate Serrano"
+      title="Frontend Developer, Designer"
+      email="kateserrano@gmail.com"
+      hashtag="#ByteMeKate"
+      avatar={avatar}
+      buttons={[
+        { src: button1, alt: "Tools" },
+        { src: button2, alt: "Folder" },
+        { src: button3, alt: "Thumbs up" },
+        { src: button4, alt: "Speech" },
+      ]}
+      tools={[
+        { src: HTML, label: "HTML" },
+        { src: CSS, label: "CSS" },
+        { src: JavaScript, label: "Javascript" },
+        { src: SQL, label: "MySQL" },
+        { src: Figma, label: "Figma" },
+      ]}
+    />
+  );
 };
 
 export default Kate;
