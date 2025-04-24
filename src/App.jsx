13a6963bd609a254ddd66703bafa7cfb254ee85e
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2025-04-24 22:02:37 +0800

    app

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,11 @@ import Play from './pages/play';
 import Shutdown from './components/shutdown';
 import Desktop from './components/Desktop/desktop';
 
-import { AudioProvider } from './contexts/AudioContext'; 
+import { AudioProvider } from './contexts/AudioContext'; //handles bg music
 
 function App() {
   useEffect(() => {
+    // redirect everything to '/' just in case someone tries to skip start page
     if (window.location.pathname !== '/') {
       window.location.replace('/');
     }
@@ -24,10 +25,10 @@ function App() {
     <AudioProvider> 
       <div className="grainy-overlay">
         <Routes>
-          <Route exact path="/" element={<Start />} />
-          <Route path="/play" element={<Play />} /> 
-          <Route path="/desktop" element={<Desktop />} />
-          <Route path="/shutdown" element={<Shutdown />}/>
+          <Route exact path="/" element={<Start />} />      {/* startup screen (auto-type text) */}
+          <Route path="/play" element={<Play />} />         {/* vinyl animation before desktop */}
+          <Route path="/desktop" element={<Desktop />} />   {/* windows 95-inspired UI */}
+          <Route path="/shutdown" element={<Shutdown />}/>  {/* shutdown animation */}
         </Routes>
       </div>
     </AudioProvider>
